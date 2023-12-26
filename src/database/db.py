d3a3b51f37ddf9d8b0376f65d18d78a779bd2142
ianMuchesia <ianmuchesia@yahoo.com>
2023-12-26 13:03:28 +0300

    alembic reviisons

--- src/database/db.py
+++ src/database/db.py
@@ -14,7 +14,6 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
--- src/models/basemodel.py
+++ src/models/basemodel.py
@@ -1,15 +1,17 @@
 import uuid
 from uuid import UUID
-from sqlalchemy import Column,  TIMESTAMP,String,Integer
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column,  TIMESTAMP,String,Integer,Boolean,ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql.expression import text
 from datetime import datetime
-from ..database.db import Base
+
+from sqlalchemy import event
 
 
 
 
 
+Base = declarative_base()
 
 class BaseModel(Base):
     __abstract__ = True
@@ -26,3 +28,48 @@ class BaseModel(Base):
         super().__init__(*args, **kwargs)
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        
+        
+        
+        
+class Job(BaseModel, Base):
+    __tablename__ = "jobs"
+
+    # No need to redefine internal_id, it is inherited from BaseModel
+    title = Column(String)
+    company = Column(String)
+    company_url = Column(String, nullable=True)
+    location = Column(String)
+    description = Column(String)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    is_active = Column(Boolean, default=True)
+    
+    user_id = Column(String, ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
+   
+    user = relationship("User")
+    @classmethod
+    def before_update_listener(cls, mapper, connection, target):
+        BaseModel.before_update_listener(mapper, connection, target)
+
+# Use event.listen with the correct event name
+event.listen(Job, 'before_update', Job.before_update_listener)
+
+
+class User(BaseModel, Base):
+    __tablename__ = "users"
+
+    # No need to redefine id and internal_id, they are inherited from BaseModel
+    username = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+
+    phone_number = Column(String, unique=True)
+    @classmethod
+    def before_update_listener(cls, mapper, connection, target):
+       BaseModel.before_update_listener(mapper, connection, target)
+
+# Register the event listener for the before_update event
+event.listen(User, 'before_update', User.before_update_listener)
